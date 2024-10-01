387bec5158883e8a4228b997fc8742843b4cab24
milcho0604 <milcho0604@gmail.com>
2024-10-01 11:04:25 +0900

    FE: 회원가입 페이지

--- src/views/member/UpdateMember.vue
+++ src/views/member/UpdateMember.vue
@@ -2,85 +2,118 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8" lg="7">
-        <v-card>
-          <v-card-title class="text-h5 text-center">회원 정보 수정</v-card-title>
+        <v-card class="position-relative custom-card">
+          <v-card-title class="text-h5 text-center">회원 가입</v-card-title>
           <v-card-text>
-            <v-form @submit.prevent="updateMemberInfo">
-              <v-text-field
-                label="이름"
-                v-model="memberEditInfo.name"
-                prepend-icon="mdi-account-box"
-                required
-              ></v-text-field>
-              <v-text-field
-                label="이메일"
-                v-model="memberEditInfo.memberEmail"
-                prepend-icon="mdi-email"
-                disabled
-              ></v-text-field>
-              <v-text-field
-                label="핸드폰 번호"
-                v-model="memberEditInfo.phoneNumber"
-                prepend-icon="mdi-phone"
-                required
-              ></v-text-field>
+            <!-- 이름 입력 -->
+            <v-text-field
+              label="이름"
+              v-model="memberEditInfo.name"
+              required
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+              style="background-color: white;"
+            ></v-text-field>
 
-              <!-- 도로명 주소 검색 버튼 -->
-              <v-text-field
-                label="주소"
-                v-model="memberEditInfo.address.city"
-                prepend-icon="mdi-home"
-                required
-                readonly
-              >
-                <template #append>
-                  <v-btn @click="openAddressSearch">주소 검색</v-btn>
-                </template>
-              </v-text-field>
+            <!-- 이메일 입력 (수정 불가) -->
+            <v-text-field
+              label="이메일"
+              v-model="memberEditInfo.memberEmail"
+              class="custom-input-field"
+              disabled
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
 
-              <v-text-field
-                label="상세주소"
-                v-model="memberEditInfo.address.street"
-                prepend-icon="mdi-home-outline"
-                required
-              ></v-text-field>
-              <v-text-field
-                label="우편번호"
-                v-model="memberEditInfo.address.zipcode"
-                prepend-icon="mdi-map-marker"
-                required
-              ></v-text-field>
+            <!-- 핸드폰 번호 입력 -->
+            <v-text-field
+              label="핸드폰 번호"
+              v-model="memberEditInfo.phoneNumber"
+              required
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
 
-              <!-- 프로필 이미지 -->
-              <v-img
-                v-if="memberEditInfo.profileImgUrl"
-                :src="memberEditInfo.profileImgUrl"
-                alt="프로필 이미지"
-                max-height="200"
-                max-width="200"
-              ></v-img>
-              <v-file-input
-                label="프로필 이미지 변경"
-                v-model="memberEditReq.profileImage"
-                prepend-icon="mdi-camera"
-                accept="image/*"
-              ></v-file-input>
+            <!-- 도로명 주소 검색 버튼 -->
+            <v-text-field
+              label="주소"
+              v-model="memberEditInfo.address.city"
+              required
+              readonly
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+            >
+              <template #append>
+                <v-btn @click="openAddressSearch" icon>
+                  <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
 
-              <v-divider class="my-4"></v-divider>
-              <v-text-field
-                label="새 비밀번호"
-                v-model="memberEditReq.password"
-                type="password"
-                prepend-icon="mdi-lock"
-              ></v-text-field>
-              <v-text-field
-                label="비밀번호 확인"
-                v-model="memberEditReq.confirmPassword"
-                type="password"
-                prepend-icon="mdi-lock-check"
-              ></v-text-field>
-              <v-btn block type="submit" color="primary">회원 정보 수정</v-btn>
-            </v-form>
+            <!-- 상세 주소 입력 -->
+            <v-text-field
+              label="상세주소"
+              v-model="memberEditInfo.address.street"
+              required
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
+
+            <!-- 우편번호 입력 -->
+            <v-text-field
+              label="우편번호"
+              v-model="memberEditInfo.address.zipcode"
+              required
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
+
+            <!-- 프로필 이미지 선택 -->
+            <v-img
+              v-if="memberEditInfo.profileImgUrl"
+              :src="memberEditInfo.profileImgUrl"
+              alt="프로필 이미지"
+              max-height="200"
+              max-width="200"
+              class="mt-3"
+            ></v-img>
+            <v-file-input
+              label="프로필 이미지 변경"
+              v-model="memberEditReq.profileImage"
+              accept="image/*"
+              class="custom-input-field"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-file-input>
+
+            <!-- 비밀번호 및 확인 -->
+            <v-divider class="my-4"></v-divider>
+            <v-text-field
+              label="새 비밀번호"
+              v-model="memberEditReq.password"
+              type="password"
+              class="custom-input-field white-input"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
+            <v-text-field
+              label="비밀번호 확인"
+              v-model="memberEditReq.confirmPassword"
+              type="password"
+              class="custom-input-field white-input"
+              persistent-hint="false"
+              hide-details="auto"
+            ></v-text-field>
+
+            <!-- 회원 가입 버튼에 @submit.prevent="updateMemberInfo" 적용 -->
+            <v-btn class="custom-btn" @click.prevent="updateMemberInfo">
+              회원 가입 완료
+            </v-btn>
           </v-card-text>
         </v-card>
       </v-col>
@@ -108,8 +141,8 @@ export default {
   name: 'MemberEditPage',
   data() {
     return {
-      dialog: false,  // 다이얼로그 상태
-      memberEditInfo: {  
+      dialog: false,
+      memberEditInfo: {
         name: '',
         memberEmail: '',
         phoneNumber: '',
@@ -118,14 +151,14 @@ export default {
           street: '',
           zipcode: ''
         },
-        profileImgUrl: ''  
+        profileImgUrl: ''
       },
-      memberEditReq: {  
+      memberEditReq: {
         password: '',
         confirmPassword: '',
-        profileImage: null 
+        profileImage: null
       },
-      originalMemberEditInfo: null 
+      originalMemberEditInfo: null
     };
   },
   created() {
@@ -136,8 +169,8 @@ export default {
       try {
         const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/member-service/member/edit-info`);
         if (response.status === 200 && response.data.result) {
-          this.memberEditInfo = response.data.result;  
-          this.originalMemberEditInfo = { ...response.data.result }; 
+          this.memberEditInfo = response.data.result;
+          this.originalMemberEditInfo = { ...response.data.result };
         } else {
           alert('회원 정보 조회에 실패했습니다.');
         }
@@ -164,7 +197,7 @@ export default {
         formData.append('address', JSON.stringify(address));
 
         if (this.memberEditReq.profileImage) {
-          formData.append('profileImage', this.memberEditReq.profileImage);  
+          formData.append('profileImage', this.memberEditReq.profileImage);
         }
 
         if (this.memberEditReq.password) {
@@ -177,7 +210,7 @@ export default {
             'Content-Type': 'multipart/form-data'
           }
         });
-        
+
         if (response.status === 200) {
           alert('회원 정보가 성공적으로 수정되었습니다.');
         } else {
@@ -191,20 +224,19 @@ export default {
       this.dialog = true;
       new window.daum.Postcode({
         oncomplete: (data) => {
-          const fullAddress = data.roadAddress;  // 전체 주소
+          const fullAddress = data.roadAddress;
           const addressParts = fullAddress.split(' ');
 
-          // 시/도, 구/군, 도로명, 상세주소 분리
           const city = addressParts[0] + ' ' + addressParts[1];
           const street = addressParts.slice(2).join(' ');
 
-          this.memberEditInfo.address.city = city;  // 시/도, 구/군
-          this.memberEditInfo.address.street = street;  // 도로명 및 상세주소
-          this.memberEditInfo.address.zipcode = data.zonecode;  // 우편번호
+          this.memberEditInfo.address.city = city;
+          this.memberEditInfo.address.street = street;
+          this.memberEditInfo.address.zipcode = data.zonecode;
           this.dialog = false;
         }
       }).open({
-        popupName: 'postcodePopup'  // 팝업 이름 지정 (optional)
+        popupName: 'postcodePopup'
       });
     }
   }
@@ -212,21 +244,64 @@ export default {
 </script>
 
 <style scoped>
+
 .v-container {
-  max-width: 900px;
+  max-width: auto;
   margin: auto;
   padding: 20px;
 }
 
-.v-card {
+.custom-card {
+  width: 509px;
+  background-color: #F3F3F3;
   border-radius: 10px;
+  position: absolute;
+  left: 58px;
+  top: 61px;
 }
-
-.v-text-field {
-  margin-bottom: 20px;
-}
-
-.v-btn {
+.custom-btn {
   font-weight: bold;
+  background-color: #C2D7FF;
+  color: #00499E;
+  border-radius: 20px;
 }
+
+.custom-input-field {
+  width: 100%;
+  background-color: #FFFFFF;  /* 모든 입력 필드에 흰색 배경 적용 */
+  border: 1px solid #E7E7E7;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  box-shadow: none;
+}
+.v-field__input {
+  background-color: #FFFFFF !important;  /* 배경을 흰색으로 설정 */
+  color: black !important;              /* 텍스트 색상을 검은색으로 설정 */
+  border: 1px solid #E7E7E7;            /* 테두리도 명시적으로 설정 */
+  border-radius: 5px;                   /* 둥근 테두리 */
+}
+.v-field__input {
+  background-color: #FFFFFF !important;  /* 배경을 흰색으로 설정 */
+  color: black !important;              /* 텍스트 색상을 검은색으로 설정 */
+}
+
+/* v-input__control 클래스에 흰색 배경을 적용 */
+.v-input__control {
+  background-color: #FFFFFF !important;  /* 컨트롤 부분 배경을 흰색으로 설정 */
+}
+
+/* v-field__field 클래스에 흰색 배경을 적용 */
+.v-field__field {
+  background-color: #FFFFFF !important;  /* 필드 부분 배경을 흰색으로 설정 */
+}
+
+/* v-input 전체 배경을 흰색으로 설정 */
+.v-input {
+  background-color: #FFFFFF !important;  /* 입력 필드 전체 영역 배경을 흰색으로 설정 */
+}
+
+.v-field__overlay {
+  background-color: #FFFFFF !important;  /* 오버레이 배경도 흰색으로 설정 */
+}
+
 </style>
