a1ac16ccd4eed0865542fe256f1329f9b82f4556
wisdom0405 <wisdom0405@kakao.com>
2024-10-07 23:17:50 +0900

    feat:  hospitalRouter 추가

---
+++ src/router/hospitalRoutr.js
@@ -0,0 +1,9 @@
+import HospitalList from "@/views/hospital/HospitalList.vue"
+
+export const hospitalRouter = [
+    {
+        path: '/hospital/list',
+        name: 'HospitalList',
+        component: HospitalList
+    }
+]
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import { memberRouter } from './memberRouter';
 import { paymentRouter } from './paymentRouter';
 import { untactRouter } from './untactRouter';
 import { chatRouter } from './chatRouter';
+import { hospitalRouter } from './hospitalRoutr';
 
 const routes = [
     {
@@ -19,6 +20,7 @@ const routes = [
     ...paymentRouter,
     ...untactRouter,
     ...chatRouter,
+    ...hospitalRouter
 ]
 
 
