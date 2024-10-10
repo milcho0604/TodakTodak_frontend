94f51d541c93a6b337facf2a1c85662fbd17f46a
milcho0604 <milcho0604@gmail.com>
2024-10-10 11:40:52 +0900

    FE: 알림 리다이렉트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,42 @@
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
-const firebaseConfig ={
-    apiKey: "AIzaSyBSH8wJ7aoblNAj8Kj7iNTfsJhlEL4KEcE",
-    authDomain: "padak-todak.firebaseapp.com",
-    projectId: "padak-todak",
-    storageBucket: "padak-todak.appspot.com",
-    messagingSenderId: "22351664979",
-    appId: "1:22351664979:web:f8a3cc4b2f5e249d88b3a6"
+const firebaseConfig = {
+  apiKey: "AIzaSyBSH8wJ7aoblNAj8Kj7iNTfsJhlEL4KEcE",
+  authDomain: "padak-todak.firebaseapp.com",
+  projectId: "padak-todak",
+  storageBucket: "padak-todak.appspot.com",
+  messagingSenderId: "22351664979",
+  appId: "1:22351664979:web:f8a3cc4b2f5e249d88b3a6"
 };
+
+// Firebase 초기화
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage((payload) => {   
-      console.log(
-        '[firebase-messaging-sw.js] Received background message ', payload);
-      const notificationTitle = payload.notification.title;
-      const notificationOptions = {
-        body: payload.notification.body,
-        icon: "favicon.ico"
-      };
-      self.registration.showNotification(notificationTitle, notificationOptions);
-    });
+// 백그라운드 메시지 수신
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const redirectUrl = payload.data.url;
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "favicon.ico"
+  };
+
+  // 알림 표시
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// 알림 클릭 시 리다이렉트 처리
+self.addEventListener('notificationclick', function(event) {
+  const redirectUrl = event.notification.data ? event.notification.data.url : null;
+  event.notification.close(); // 알림 닫기
+  if (redirectUrl) {
+    event.waitUntil(
+      clients.openWindow(redirectUrl) // URL로 리다이렉트
+    );
+  }
+});
