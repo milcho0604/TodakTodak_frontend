753987cf232635afe040a787d2c667137283f29b
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-10-11 14:07:23 +0900

    fix: 자녀 모달 수정

--- src/views/child/ChildShareModal.vue
+++ src/views/child/ChildShareModal.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- 자녀 공유 모달 -->
     <v-dialog v-model="childDeleteDialog" max-width="450px">
-        <v-card class="modal">
+        <v-card class="child-modal">
             <v-row class="mt-3" justify="center">
                 <div class="headline inter-bold">자녀정보 공유</div>
             </v-row>
@@ -103,7 +103,7 @@ export default {
 </script>
 
 <style scoped>
-.modal {
+.child-modal {
     padding: 20px;
     border-radius: 40px;
 }
