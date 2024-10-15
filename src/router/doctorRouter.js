16dc3b338a9ff7fc3fa3e77987628a5b4ef0e20b
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-10-15 12:53:21 +0900

    feat: 의사 비대면 예약 관리 페이지

---
+++ src/router/doctorRouter.js
@@ -0,0 +1,9 @@
+import DoctorReservation from "@/views/doctor/DoctorReservation.vue";
+
+export const doctorRouter = [
+    {
+        path: '/doctor/reservation',
+        name: 'DoctorReservation',
+        component: DoctorReservation
+    },
+]
