72e18e4c6b0508dccb30283369d8f882c4b5afa7
milcho0604 <milcho0604@gmail.com>
2024-10-24 17:08:41 +0900

    FE: 병원관리자 디테일 페이지 전화번호 숫자만 입력

--- src/views/hospitalAdmin/HospitaAdminHospitallDetail.vue
+++ src/views/hospitalAdmin/HospitaAdminHospitallDetail.vue
@@ -73,12 +73,13 @@
   
           <h6 class="text-left">병원 번호</h6>
           <v-text-field
-            v-model="hospital.phoneNumber"
-            variant="underlined"
-            label="병원 전화번호를 입력해주세요."
-            :rules="[rules.required]"
-            :readonly="!isEditing"
-          />
+          v-model="hospital.phoneNumber"
+          variant="underlined"
+          label="병원 전화번호를 입력해주세요."
+          :rules="[rules.required]"
+          :readonly="!isEditing"
+          @input="hospital.phoneNumber = hospital.phoneNumber.replace(/[^0-9]/g, '')"
+        />
   
           <h6 class="text-left">비대면 진료비</h6>
           <v-text-field
