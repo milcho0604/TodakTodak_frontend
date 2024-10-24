905ff0deb3a13b8c67477ca096c030d0a276180a
milcho0604 <milcho0604@gmail.com>
2024-10-24 17:40:18 +0900

    feat: 병원 디테일 수정

--- src/views/hospitalAdmin/HospitaAdminHospitallDetail.vue
+++ src/views/hospitalAdmin/HospitaAdminHospitallDetail.vue
@@ -75,7 +75,7 @@
           <v-text-field
           v-model="hospital.phoneNumber"
           variant="underlined"
-          label="병원 전화번호를 입력해주세요."
+          label="병원 전화번호를 입력해주세요.(ex.0212341234)"
           :rules="[rules.required]"
           :readonly="!isEditing"
           @input="hospital.phoneNumber = hospital.phoneNumber.replace(/[^0-9]/g, '')"
@@ -140,7 +140,7 @@
 
           <v-textarea
           v-model="hospital.notice"
-          label="병원 소개를 입력해주세요."
+          label="병원 공지를 입력해주세요."
           class="hospital-card"
           :readonly="!isEditing"
           :rules="[rules.required]"
