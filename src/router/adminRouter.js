fc4aa139376b33ca0c2ed22a3c4f685fa7935cb4
wisdom0405 <wisdom0405@kakao.com>
2024-10-26 17:53:37 +0900

    fix : CS고치는중

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -7,26 +7,31 @@ import AdminCsChat from "@/views/todak/AdminCsChat.vue";
 
 export const adminRouter = [
     {
+        // 회원리스트
         path: '/admin/member/list',
         name: 'MemberList',
         component: MemberList
     },
     {
+        // 병원리스트
         path: '/admin/hospital/list',
         name: 'HospitalAdminList',
         component: HospitalAdminList
     },
     {
+        // 병원승인
         path: '/admin/hospital/detail/:id',
         name: 'HospitalAdminDetail',
         component: HospitalAdminDetail
     },
     {
+        // 결제내역 리스트
         path: '/admin/payment/list',
         name: 'PaymentList',
         component: PaymentList
     },
     {
+        // 상담내역 CS 리스트
         path: '/admin/cs/list',
         name: 'CsList',
         component: CsList
