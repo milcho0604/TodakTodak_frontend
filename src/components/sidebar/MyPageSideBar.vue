c747f92bc833d1910d240b7627d5725fa091b940
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-10-29 12:48:34 +0900

    fix: 헤더 사이드바 수정

--- src/components/sidebar/MyPageSideBar.vue
+++ src/components/sidebar/MyPageSideBar.vue
@@ -3,7 +3,7 @@
     v-model="drawer"
     :rail="rail"
     permanent
-    style="background-color: #ECF2FE;"
+    style="background-color: #FCFCFC;"
     @click="rail = false"
     >
     <v-list-item
