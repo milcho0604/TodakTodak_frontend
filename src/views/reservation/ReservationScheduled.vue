95865bacf574f9a4d1753676951f17f3bf81a102
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-11-01 11:53:04 +0900

    배포 테스트 6차

--- src/views/reservation/ReservationScheduled.vue
+++ src/views/reservation/ReservationScheduled.vue
@@ -449,6 +449,7 @@ export default {
         },
         async fetchDoctorList() {
             try {
+                console.log(this.hospitalId);
                 const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/member-service/member/doctorList/${this.hospitalId}`);
                 this.doctorList = response.data.result.content;
                 console.log(response)
