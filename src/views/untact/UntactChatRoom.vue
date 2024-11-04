c6cbf19b139f49ce03e71f0c5e3fb7421e45ac6e
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-11-04 15:23:18 +0900

    fix :에러 처리 수정

--- src/views/untact/UntactChatRoom.vue
+++ src/views/untact/UntactChatRoom.vue
@@ -231,10 +231,10 @@ export default {
       navigator.mediaDevices.getUserMedia({ audio: true, video: true })
         .then(stream => {
           this.localStream = stream;
-            // 오디오 트랙을 무음 처리하여 로컬 비디오에 오디오가 나오지 않도록 설정
-            this.localStream.getAudioTracks().forEach(track => {
-              track.enabled = false; // 트랙은 생성하지만 로컬 오디오 출력을 막음
-            });
+          // 오디오 트랙을 무음 처리하여 로컬 비디오에 오디오가 나오지 않도록 설정
+          this.localStream.getAudioTracks().forEach(track => {
+            track.enabled = false; // 트랙은 생성하지만 로컬 오디오 출력을 막음
+          });
           this.$refs.localVideo.srcObject = stream;
           stream.getTracks().forEach(track => this.myPeerConnection.addTrack(track, this.localStream));
         });
@@ -416,16 +416,22 @@ export default {
         alert("진료가 종료되었습니다.");
         this.reviewModal = true;
       } else if (localStorage.getItem('role') === 'DOCTOR') {
-        try {
-          // updateStatus가 완료될 때까지 기다립니다.
-          await this.updateStatus('Completed');
-          // updateStatus가 성공적으로 완료되면 페이지 이동
-          window.location.href = '/doctor/untact/reservation';
-        } catch (error) {
+        // 진료 종료시 예약 상태 & 진료 내역 상태 진료완료로 업데이트
+        const req = {
+          id: this.sid,
+          status: 'Completed'
+        }
+        await axios.post(`${process.env.VUE_APP_API_BASE_URL}/reservation-service/reservation/hospital/untact/update`,
+          req
+        ).then(response => {
+          if (response.status === 200 && response.data.result) {
+            window.location.href = '/doctor/untact/reservation';
+          }
+        }).catch(error => {
           // updateStatus가 실패하면 오류 메시지를 출력하고 페이지 이동하지 않음
           console.error("Status update failed:", error);
           alert("진료 상태 업데이트에 실패했습니다. 다시 시도해주세요.");
-        }
+        });
       }
     },
     handlePeerLeave(message) {
@@ -466,22 +472,6 @@ export default {
         .catch(error => {
           console.error('Error creating medical chart:', error);
         });
-    },
-    // 진료 종료시 예약 상태 & 진료 내역 상태 진료완료로 업데이트
-    async updateStatus(data) {
-      console.log(" 진료 완료처리 하려는데 ...")
-      try {
-        const req = {
-          id: this.sid,
-          status: data
-        }
-        console.log(req);
-        await axios.post(`${process.env.VUE_APP_API_BASE_URL}/reservation-service/reservation/hospital/untact/update`,
-          req
-        )
-      } catch (e) {
-        console.log(e)
-      }
     },
     openPayModal() {
       console.log("이제 결제할게");
